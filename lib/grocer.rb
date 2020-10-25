ece85c8aeef7e6571f135432bfb039322c3166f9
Jeremy Armah <jarmah241@gmail.com>
2020-10-25 20:31:11 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -20,7 +20,15 @@ def consolidate_cart(cart)
   
   new_cart = []
   cart.each do |item|
-    p item[:item]
+    if cart.count(item) == 1 
+      item[:count] = 1
+      new_cart << item
+    else
+      if find_item_by_name_in_collection(item[:item], cart) == nil
+        item[:count] = cart.count(item)
+        new_cart << item
+      end
+    end
   end
   p new_cart
   
