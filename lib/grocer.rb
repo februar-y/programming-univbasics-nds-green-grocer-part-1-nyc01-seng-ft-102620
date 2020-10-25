36a3c0a1293d7c251dd88838ac496a04dd6e6c45
Jeremy Armah <jarmah241@gmail.com>
2020-10-25 20:35:20 +0000

    Done.

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -2,7 +2,14 @@ def find_item_by_name_in_collection(name, collection)
   # Implement me first!
   #
   # Consult README for inputs and outputs
-
+  
+  item_found = nil
+  collection.each do |item|
+    if item[:item] == name
+      item_found = item
+    end
+  end
+  item_found
 end
 
 def consolidate_cart(cart)
@@ -10,7 +17,15 @@ def consolidate_cart(cart)
   #
   # REMEMBER: This returns a new Array that represents the cart. Don't merely
   # change `cart` (i.e. mutate) it. It's easier to return a new thing.
-
+  
+  new_cart = []
+  cart.each do |item|
+    if find_item_by_name_in_collection(item[:item], new_cart) == nil
+      item[:count] = cart.count(item)
+      new_cart << item
+    end
+  end
+  new_cart
 end
 
 
